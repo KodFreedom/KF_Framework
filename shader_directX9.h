affffd4995ebe8022d286ab87b4c41c475a52eab
KodFreedom <kodfreedom@gmail.com>
2017-12-01 13:41:43 +0900

    created super class

---
+++ shader_directX9.h
@@ -0,0 +1,38 @@
+//--------------------------------------------------------------------------------
+//　shader_directX9.h
+//	シェーダークラス
+//	Author : 徐文杰(KodFreedom)
+//--------------------------------------------------------------------------------
+#pragma once
+#include "common_setting.h"
+#if defined(USING_DIRECTX) && (DIRECTX_VERSION == 9)
+//--------------------------------------------------------------------------------
+//  前方宣言
+//--------------------------------------------------------------------------------
+class MeshRenderer;
+
+//--------------------------------------------------------------------------------
+//  基底クラス
+//--------------------------------------------------------------------------------
+class ShaderDirectX9
+{
+public:
+	ShaderDirectX9()
+		: vertex_shader_(nullptr)
+		, vertex_shader_constant_table_(nullptr)
+		, pixel_shader_(nullptr)
+		, pixel_shader_constant_table_(nullptr) {}
+	~ShaderDirectX9() {}
+
+	virtual void Init(void) = 0;
+	virtual void Uninit(void) = 0;
+	virtual void Set(const LPDIRECT3DDEVICE9 device) = 0;
+	virtual void SetConstantTable(const MeshRenderer& renderer) = 0;
+
+protected:
+	LPDIRECT3DVERTEXSHADER9	vertex_shader_;
+	LPD3DXCONSTANTTABLE     vertex_shader_constant_table_;
+	LPDIRECT3DPIXELSHADER9	pixel_shader_;
+	LPD3DXCONSTANTTABLE		pixel_shader_constant_table_;
+};
+#endif
