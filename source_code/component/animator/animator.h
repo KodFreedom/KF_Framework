3e6ba97afdfe3625d85eb56fcf4b0e3fc5555df1
KodFreedom <kodfreedom@gmail.com>
2018-02-01 02:22:15 +0900

    ソースコードをフォルダごとに整理した

--- source_code/component/animator/animator.h
+++ source_code/component/animator/animator.h
@@ -4,10 +4,10 @@
 //  Author : Xu Wenjie
 //--------------------------------------------------------------------------------
 #pragma once
-#include "component.h"
-#include "kf_math.h"
+#include "../component.h"
+#include "../../KF_Math.h"
 using namespace kodfreedom;
-#include "motion_state\\motion_state.h"
+#include "../../motion_state/motion_state.h"
 
 //--------------------------------------------------------------------------------
 //  ëOï˚êÈåæ
